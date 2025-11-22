b8c5d08708596cdf22e399dcbe7a84d3acb80c77
tajo9128 <tajo9128@gmail.com>
2025-11-22 14:21:31 +0530

    Fix success stories view to prevent 500 error

--- community/success_views.py
+++ community/success_views.py
@@ -1,8 +1,11 @@
 from django.views.generic import ListView
-from .success_models import SuccessStory
+from .models import Badge
 
 class SuccessStoriesView(ListView):
-    model = SuccessStory
+    model = Badge  # Placeholder - using Badge model for now
     template_name = 'community/success_stories.html'
     context_object_name = 'stories'
-    ordering = ['-created_at']
+    
+    def get_queryset(self):
+        # Return empty queryset for now since we don't have a SuccessStory model yet
+        return Badge.objects.none()
